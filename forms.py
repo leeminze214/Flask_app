42e8ee4b7760b348d0d5438358efa48b4c468bbe
LEE ZHENG <61095332+leeminze214@users.noreply.github.com>
2020-08-24 16:57:28 -0400

    Update forms.py
    getting rid of old json file code

--- forms.py
+++ forms.py
@@ -11,9 +11,4 @@ class login_form(FlaskForm):
     pw = StringField("Password")
     submit = SubmitField("click to login")
 
-def update_database(data):
-    with open("db.json",'w') as f:
-        json.dump(data, f)
-def open_db():
-    with open("db.json") as f:
-        return json.load(f)
+
